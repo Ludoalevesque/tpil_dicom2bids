79017e23f9f79b844d0046053c55d2dc8ea96a38
Ludoalevesque <ludovic.arsenault-levesque@polymtl.ca>
2023-07-20 10:51:25 -0400

    update copy_dicom

--- copy_dicom.sh
+++ copy_dicom.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Source directory (your hard drive root directory)
-source_directory="/path/to/source"
+source_directory="/Volumes/G-DRIVE PRO/IRM_humain"
 
 # Destination directory
-destination_directory="/path/to/destination"
+destination_directory="/Volumes/PT_LAB_DATA/Data_ludo/DICOM_all_sub/"
+
+if [ ! -d "$source_directory"]
+
+#cp -r  "$source_directory" "$destination_directory"
 
 # Find all DICOM subfolders and copy them to the destination directory
 find "$source_directory" -type d -name "DICOM" -exec cp -R --parents {} "$destination_directory" \;
