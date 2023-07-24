9a2d8127b259cd1d1dc1a1097223edf694971f99
Ludoalevesque <ludovic.arsenault-levesque@polymtl.ca>
2023-07-24 10:02:53 -0400

    Updating somefiles

--- run_dcm2bids_all_subs.sh
+++ run_dcm2bids_all_subs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Source directory 
+# Configuration file
+config_file="/path/to/your/config_file.json"  # Replace with the actual path of your config file
+
+# Source directory
 source_directory="/Volumes/G-DRIVE PRO/IRM_humain"
 
 # Destination directory
@@ -23,23 +26,25 @@ for folder_path in "$source_directory"/*/; do
   # Extract the folder name without the full path
   folder_name=$(basename "$folder_path")
 
-  
   # Check if the subfolder "DICOM" exists in the current folder
-  if [  ! -d "$dicom_dir" ]; then
+  dicom_dir="$folder_path/DICOM"  # Define the path to the DICOM folder
+  if [ ! -d "$dicom_dir" ]; then
     echo "No DICOM folder found in source folder $folder_name"
     exit 1
   fi
 
   # Extract the subject ID (3 digit number that follows PT_PL in foldername)
-  sub_ID=
+  sub_ID=$(echo "$folder_name" | grep -oE 'PT_PL[0-9]{3}' | grep -oE '[0-9]{3}')
 
   # Extract the session number (all the numbers that follow V in folder_name)
-  session=
+  session=$(echo "$folder_name" | grep -oE 'V[0-9]+')
 
   # Convert DICOM data to BIDS
+  output_path="${destination_directory}/$session_BIDS"
+  dcm2bids -d "$dicom_dir" -o "$output_path" -p "$sub_ID" -c "$config_file"
 
-  output_path="${destination_directory}/V${session}_BIDS"
+echo "Finished conversion for subject ${session}"
 
-  dcm2bids -d "$dicom_dir" -o "$destination_directory" -p "$sub_ID"
+done
 
-echo "Finished copying DICOM folders."
+echo "Finished conversion"
